8451ab893aba0ccbaa34665d6a87c70618d6329d
Klim <132146883+Applecmerte1@users.noreply.github.com>
2025-06-26 22:09:40 +0300

    nothin cool

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from about_me import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main_page)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
