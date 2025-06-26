7280051e5e22b935a465dfc077b5ea1255475308
Klim <132146883+Applecmerte1@users.noreply.github.com>
2025-06-26 22:31:36 +0300

    add images

--- about_me/views.py
+++ about_me/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Project
 
 def main_page(request):
-    return render(request, 'about_me/main.html')
+    projects = Project.objects.all()
+    return render(request, 'about_me/main.html', {'projects':projects})
