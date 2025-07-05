65bf4710adacf520a85c2419acbc85e3fca4a3f0
Klim <132146883+Applecmerte1@users.noreply.github.com>
2025-07-05 23:03:10 +0300

    ахуеть, комментарий

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from .models import Blog_project
 
 def all_blogs(request):
     blogs = Blog_project.objects.all()
+    #для ограничения постов писать order_by('-date')[:5] (сортировка по дате)
     return render(request,'blog/all_blogs.html',{'blogs':blogs})
