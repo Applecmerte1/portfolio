e06112f6b8b5697f6f467d0a7d8998ad69b46337
Klim <132146883+Applecmerte1@users.noreply.github.com>
2025-07-05 22:45:02 +0300

    пока работает сделаю комит, а то очково пиздец

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from .models import Blog_project
+
+admin.site.register(Blog_project)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog_project
 
-# Create your views here.
+def all_blogs(request):
+    blogs = Blog_project.objects.all()
+    return render(request,'blog/all_blogs.html',{'blogs':blogs})
