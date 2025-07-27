6aa362f95fe6468e1314d5086ec51525ea049242
Klim <132146883+Applecmerte1@users.noreply.github.com>
2025-07-27 22:24:15 +0300

    урод, но рабочий

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog_project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
