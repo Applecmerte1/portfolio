567242e1d0ef7bdd3c19e4b17a281f8c85237c2f
Klim <132146883+Applecmerte1@users.noreply.github.com>
2025-07-27 21:53:53 +0300

    сделал функциональность у постов, но не понимаю как   это работает

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog_project
 
 def all_blogs(request):
     blogs = Blog_project.objects.all()
     #для ограничения постов писать order_by('-date')[:5] (сортировка по дате)
     return render(request,'blog/all_blogs.html',{'blogs':blogs})
+def detail(request,blog_id):
+    blog = get_object_or_404(Blog_project,pk = blog_id)
+    return render(request,'blog/detail.html',{'blog':blog})
